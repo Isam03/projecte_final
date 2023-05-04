2e59bd1869a4a2f49930bd754cbdb07f081c21dd
Isam <ihajji.dam@institutcampalans.net>
2023-05-04 18:54:38 +0200

    registro y login funcionando

---
+++ models/usuario.js
@@ -0,0 +1,68 @@
+const { json } = require("express");
+const mongoose = require("mongoose");
+const bcrypt = require('bcryptjs');
+
+const UsuarioSchema = new mongoose.Schema({
+
+    nombre: {
+        type: String,
+        required: true
+    },
+    apellido: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    dni: {
+        type: String,
+        required: true
+    },
+    fecha_nacimiento: {
+        type: Date,
+        required: true
+    },
+    rol: {
+        type: Number,
+        required: false,
+        default: 2
+    }
+
+
+});
+
+UsuarioSchema.pre('save', async function(next) {
+    try {
+        // check method of registration
+        const user = this;
+        if (!user.isModified('password')) next();
+        // generate salt
+        const salt = await bcrypt.genSalt(10);
+        // hash the password
+        const hashedPassword = await bcrypt.hash(this.password, salt);
+        // replace plain text password with hashed password
+        this.password = hashedPassword;
+        next();
+    } catch (error) {
+        return next(error);
+    }
+});
+
+// UsuarioSchema.methods.encryptPassword = async password=>{
+//     const salt = await bcrypt.genSalt(10);
+//     return await bcrypt.hash(password,salt);
+// };
+
+UsuarioSchema.methods.matchPassword = async function(password){
+    return await bcrypt.compare(password, this.password);
+}
+
+const Usuario = mongoose.model("usuarios", UsuarioSchema);
+
+module.exports = Usuario;
---
+++ server.js
@@ -0,0 +1,124 @@
+// API REST per el manteniment de verdures 
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const axios = require('axios');
+const session = require("express-session");
+const passport = require("passport");
+const bodyParser = require("body-parser");
+const usuarios = require('./models/usuario');
+var moment = require('moment');
+
+const app = express();
+const port = 3000;
+app.use(express.json());
+
+mongoose.connect('mongodb+srv://isam:isam03@parking.ikyce.mongodb.net/quedaya',
+{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", ()=>{
+    console.log("Connected successfully");
+});
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,'www')));
+
+app.use(session({
+    secret: 'testests',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60 * 60 * 1000 } // 1 hour
+  }));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(usuarios.createStrategy());
+
+passport.serializeUser(usuarios.serializeUser());
+passport.deserializeUser(usuarios.deserializeUser());
+
+require('./api')(app);
+
+///////////////////////////////////////////////////
+////////////////// VISTES ////////////////////
+///////////////////////////////////////////////////
+
+// // View engine setup
+// app.set('view engine', 'html');
+app.set('views', './www/views')
+app.set('view engine', 'ejs')
+
+// // With middleware
+// app.use('/api/actividades', function (req, res, next) {
+    
+//     next();
+// });
+
+/// VISTA de INICIO
+app.get('/', async (req, res) => {
+    try {
+        const response = await axios.get('http://localhost:3004/api/actividades');
+        const data = response.data;
+        res.render('index', {events: data});
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Server Error');
+    }
+})
+
+/// VISTA de LOGIN / REGISTRO
+app.get('/login', (req, res) => {
+    res.render('login')
+})
+
+//////////////////////////////////////////////////
+///////////////   AUTENTICACIÓ   /////////////////
+//////////////////////////////////////////////////
+
+// POST LOGIN
+
+// app.post("/login", async (req,res) =>{
+
+// })
+
+app.post('/login', passport.authenticate('local', { failureRedirect: '/' }),  function(req, res) {
+	console.log(req.usuarios)
+	res.redirect('/');
+});
+
+app.get("/user/protected", passport.authenticate("local", { session: false }),
+    (req, res, next) => {
+        res.json({user: req.user});
+    }
+);
+
+app.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
+});
+
+
+/// VISTA de EVENTO
+app.get('/event', (req, res) => {
+    res.render('event/event')
+})
+
+/// VISTA de CREAR EVENTO
+app.get('/event/create', (req, res) => {
+    res.render('event/crear')
+})
+
+
+
+
+app.listen(port, () => {
+    console.log(`Servidor arrancat, escoltant el port ${port}`);
+}); 
