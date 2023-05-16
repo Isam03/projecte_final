569125186e65364acccd1635fa4d48b31dd00754
MichiSus <105300656+Mish2013@users.noreply.github.com>
2023-05-16 15:47:46 +0200

    adsadas

--- models/usuario.js
+++ models/usuario.js
@@ -48,16 +48,19 @@ const UsuarioSchema = new mongoose.Schema({
 
 UsuarioSchema.pre('save', async function(next) {
     try {
-        // check method of registration
-        const user = this;
-        if (!user.isModified('password')) next();
-        // generate salt
-        const salt = await bcrypt.genSalt(10);
-        // hash the password
-        const hashedPassword = await bcrypt.hash(this.password, salt);
-        // replace plain text password with hashed password
-        this.password = hashedPassword;
-        next();
+        if(this.password > 1){
+            // check method of registration
+            const user = this;
+            if (!user.isModified('password')) next();
+            // generate salt
+            const salt = await bcrypt.genSalt(10);
+            // hash the password
+            const hashedPassword = await bcrypt.hash(this.password, salt);
+            // replace plain text password with hashed password
+            this.password = hashedPassword;
+            next();
+        }
+        
     } catch (error) {
         return next(error);
     }
